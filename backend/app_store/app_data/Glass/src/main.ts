9b74130323aef1aa2c3e614e6963256fd9c5618f
kellentow <kellentow12@gmail.com>
2025-10-11 01:04:16 -0500

    Enhance IPC interface and update GlassWindow structure; add 'glass' to app list

--- backend/app_store/app_data/Glass/src/main.ts
+++ backend/app_store/app_data/Glass/src/main.ts
@@ -1,18 +1,23 @@
+interface IPC {
+    send: (data:any) => void;
+    recv: () => any | undefined;
+}
+
 interface Window {
-    parent_doc: () => Document | undefined,
-    os: any,
-    proc: any,
-    IPCs: any[],
+    parent_doc: () => Document | undefined;
+    os: any;
+    proc: any;
+    IPCs: IPC[];
 }
 
 interface GlassWindow {
-    id: string;
     element: HTMLElement;
     x: number;
     y: number;
     width: number;
     height: number;
     z: number;
+    ipc: IPC;
 }
 
 let windows: GlassWindow[] = [];
@@ -22,3 +27,33 @@ window.proc.requestPermission(4,"Permission to edit the DOM is required to show 
 // Overwrite global document to point to parent_doc
 document = window.parent_doc() as Document;
 if (!document) {throw new Error("User refused DOM access")};
+
+function IPCHandler(ipc:IPC,window:GlassWindow) {
+    let msg:any = ""
+    while (msg !== undefined) {
+        try {
+            msg = ipc.recv()
+            if (msg === undefined) {
+                break
+            }
+        
+            if (msg.type == "pos") {
+                window.x = msg.x
+                window.y = msg.y
+            } else if (msg.type == "size") {
+                window.width = msg.width
+                window.height = msg.height
+            } else if (msg.type == "element") {
+                const elem = document.importNode(msg.element, true)
+                window.element = elem
+            } 
+        } catch {
+
+        }
+    }
+}
+
+function updateLoop() {
+    requestAnimationFrame(updateLoop)
+}
+requestAnimationFrame(updateLoop)
